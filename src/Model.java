db0ef4aac444a58d017253ce6fd51b8b47ec4ee1
Andrea Mourino <amourinodesousa@danielcastelao.org>
2025-06-18 13:22:51 +0200

    realizando calculo en model

--- src/Model.java
+++ src/Model.java
@@ -1,18 +1,18 @@
 public class Model {
 
-    public static double sumarM(double numero1, double numero2) {
+    public static double sumarNumerosM(double numero1, double numero2) {
         return numero1 + numero2;
     }
 
-    public static double restarM(double numero1, double numero2) {
+    public static double restarNumerosM(double numero1, double numero2) {
         return numero1 - numero2;
     }
 
-    public static double multiplicarM(double numero1, double numero2) {
+    public static double multiplicarNumerosM(double numero1, double numero2) {
         return numero1 * numero2;
     }
 
-    public static double dividirM(double numero1, double numero2) {
+    public static double dividirNumerosM(double numero1, double numero2) {
         return numero1 / numero2;
     }
 }
