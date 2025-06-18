29a208590da5d56fbff7bfef2396e6fa15bb1e8d
Andrea Mourino <amourinodesousa@danielcastelao.org>
2025-06-18 13:23:31 +0200

    agregando valores double numero1 y numero2

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,13 +3,16 @@ public class Calculadora {
     int resta;
     int multiplicacion;
     int division;
-    //double numero;
+    double numero1;
+    double numero2;
 
-    public Calculadora(int suma, int resta, int multiplicacion, int division) {
-        this.suma = 0;
-        this.resta = 0;
-        this.multiplicacion = 0;
-        this.division = 0;
+    public Calculadora(int suma, int resta, int multiplicacion, int division, double numero1, double numero2) {
+        this.suma = suma;
+        this.resta = resta;
+        this.multiplicacion = multiplicacion;
+        this.division = division;
+        this.numero1= 0;
+        this.numero2= 0;
 
 
     }
