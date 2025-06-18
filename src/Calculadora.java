35745adf8a4edf09ad0c18518f60c14c9713776a
Andrea Mourino <amourinodesousa@danielcastelao.org>
2025-06-18 13:01:32 +0200

    creacion de los valores

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,2 +1,16 @@
 public class Calculadora {
+    int suma;
+    int resta;
+    int multiplicacion;
+    int division;
+    //double numero;
+
+    public Calculadora(int suma, int resta, int multiplicacion, int division) {
+        this.suma = 0;
+        this.resta = 0;
+        this.multiplicacion = 0;
+        this.division = 0;
+
+
+    }
 }
