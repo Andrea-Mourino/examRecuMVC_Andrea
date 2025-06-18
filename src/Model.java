777ad72268e53375f7f90f88d7fda222e961bba4
Andrea Mourino <amourinodesousa@danielcastelao.org>
2025-06-18 13:03:03 +0200

    creacion de los parametros del model

--- src/Model.java
+++ src/Model.java
@@ -1,2 +1,18 @@
 public class Model {
+
+    public static double sumarM(double a, double b) {
+        return a + b;
+    }
+
+    public static double restarM(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicarM(double a, double b) {
+        return a * b;
+    }
+
+    public static double dividirM(double a, double b) {
+        return a / b;
+    }
 }
