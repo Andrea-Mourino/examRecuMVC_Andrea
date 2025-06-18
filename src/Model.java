955eedfc9f2b4b5895c190286bc8f434ff210819
Andrea Mourino <amourinodesousa@danielcastelao.org>
2025-06-18 13:07:18 +0200

    arreglando nombres valores

--- src/Model.java
+++ src/Model.java
@@ -1,18 +1,18 @@
 public class Model {
 
-    public static double sumarM(double a, double b) {
-        return a + b;
+    public static double sumarM(double numero1, double numero2) {
+        return numero1 + numero2;
     }
 
-    public static double restarM(double a, double b) {
-        return a - b;
+    public static double restarM(double numero1, double numero2) {
+        return numero1 - numero2;
     }
 
-    public static double multiplicarM(double a, double b) {
-        return a * b;
+    public static double multiplicarM(double numero1, double numero2) {
+        return numero1 * numero2;
     }
 
-    public static double dividirM(double a, double b) {
-        return a / b;
+    public static double dividirM(double numero1, double numero2) {
+        return numero1 / numero2;
     }
 }
