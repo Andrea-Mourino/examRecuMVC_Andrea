fc3ec25457fc68caecf3bbe7ca831bff70426ca9
Andrea Mourino <amourinodesousa@danielcastelao.org>
2025-06-18 13:00:24 +0200

    crear static int

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,19 @@
 public class Controller {
+
+    public static int sumaC(int suma) {
+        return Model.sumarNumeros(suma);
+    }
+
+    public static int restarC(int resta) {
+        return Model.restarNumeros(resta);
+    }
+
+    public static int multiplicacionC(int multiplicacion) {
+        return Model.multiplicarNumeros(multiplicacion);
+    }
+
+    public static int divisionC(int division) {
+        return Model.dividirNumeros(division);
+    }
+
 }
